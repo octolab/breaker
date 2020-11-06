05e0991a4495cc9db4c0a45408b287cb11667ddf
Kamil Samigullin <kamil@samigullin.info>
2020-11-06 22:39:03 +0000

    merge context into base

--- breaker.go
+++ breaker.go
@@ -2,12 +2,24 @@
 package breaker
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"sync"
 	"sync/atomic"
 	"time"
 )
+
+// BreakByContext returns a new Breaker based on the Context.
+//
+//  interrupter := breaker.BreakByContext(context.WithTimeout(req.Context(), time.Minute)),
+//  defer interrupter.Close()
+//
+//  background.Job().Do(interrupter)
+//
+func BreakByContext(ctx context.Context, cancel context.CancelFunc) Interface {
+	return (&contextBreaker{ctx, cancel}).trigger()
+}
 
 // BreakByDeadline closes the Done channel when the deadline occurs.
 func BreakByDeadline(deadline time.Time) Interface {
@@ -78,6 +90,30 @@ func (br *breaker) Released() bool {
 }
 
 func (br *breaker) trigger() Interface {
+	return br
+}
+
+type contextBreaker struct {
+	context.Context
+	cancel context.CancelFunc
+}
+
+// Close closes the Done channel and releases resources associated with it.
+func (br *contextBreaker) Close() {
+	br.cancel()
+}
+
+// Released returns true if resources associated with the Breaker were released.
+func (br *contextBreaker) Released() bool {
+	select {
+	case <-br.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+func (br *contextBreaker) trigger() Interface {
 	return br
 }
 
--- breaker_test.go
+++ breaker_test.go
@@ -1,6 +1,7 @@
 package breaker_test
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"syscall"
@@ -9,6 +10,53 @@ import (
 
 	. "github.com/kamilsk/breaker"
 )
+
+func TestBreakByContext(t *testing.T) {
+	t.Run("active breaker", func(t *testing.T) {
+		br := BreakByContext(context.WithTimeout(context.Background(), 5*delta))
+		if isReleased(br) {
+			t.Error("a breaker is released")
+		}
+
+		start := time.Now()
+		<-br.Done()
+
+		checkDuration(t, start.Add(5*delta), time.Now())
+		checkBreakerIsReleased(t, br)
+	})
+
+	t.Run("closed breaker", func(t *testing.T) {
+		br := BreakByContext(context.WithTimeout(context.Background(), -delta))
+		start := time.Now()
+		<-br.Done()
+
+		checkDuration(t, start, time.Now())
+		checkBreakerIsReleased(t, br)
+	})
+
+	t.Run("released breaker", func(t *testing.T) {
+		br := BreakByContext(context.WithTimeout(context.Background(), time.Hour))
+		br.Close()
+		start := time.Now()
+		<-br.Done()
+
+		checkDuration(t, start, time.Now())
+		checkBreakerIsReleased(t, br)
+	})
+
+	t.Run("canceled context", func(t *testing.T) {
+		var (
+			ctx, cancel = context.WithTimeout(context.Background(), time.Hour)
+			br          = BreakByContext(ctx, cancel)
+		)
+		cancel()
+		start := time.Now()
+		<-br.Done()
+
+		checkDuration(t, start, time.Now())
+		checkBreakerIsReleased(t, br)
+	})
+}
 
 func TestBreakByDeadline(t *testing.T) {
 	t.Run("future deadline", func(t *testing.T) {
